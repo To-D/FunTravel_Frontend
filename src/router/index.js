58d8d4bfb18c84ad6b2c3d3f2cc19eba30fa4885
To-D <1244467220@qq.com>
2020-07-13 17:54:25 +0800

    [New]First push

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/pages/Login'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [{
+        path: '/',
+        name: "Login",
+        component: Login
+    }]
+})
